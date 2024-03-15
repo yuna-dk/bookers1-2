00c8ffe8068a680e36ffc4b90e294c096d0ca6a6
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2024-03-15 03:14:01 +0000

    [Finish]bookers二回目

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book)
     else
-      flash[:notice] = "An error has occurred."
+      flash.now[:alert] = "An error has occurred."
       render :new
     end
   end
@@ -26,26 +26,33 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
-      flash.now[:notice] = "An error has occurred."
+      flash.now[:alert] = "An error has occurred."
       render :edit
     end
   end
-  
+
   def destroy
-    
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = "Book was successfully updated."
+      redirect_to books_path
+    else
+      flash.now[:alert] = "An error has occurred."
+      render :index
+    end
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
